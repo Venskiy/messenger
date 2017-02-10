ac8f9efdc4f02b8873e6b17e78ec6080064df4cd
vensilya@gmail.com <vensilya@gmail.com>
2017-02-10 14:04:52 +0300

    Init home route

--- App/routes/Home.js
+++ App/routes/Home.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 
-import Home from './routes/Home'
-
 export default class Index extends Component {
   render() {
     return (
-      <Home />
+      <View>
+        <Text>
+          Home route
+        </Text>
+      </View>
     );
   }
 }
