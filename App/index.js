694e58caa34713450c65c7770d31a57fd072fae0
vensilya@gmail.com <vensilya@gmail.com>
2017-02-10 14:12:19 +0300

    Init  chat route

--- App/index.js
+++ App/index.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 
-import Home from './routes/Home'
+import Home from './routes/Home';
+import Chat from './routes/Chat';
 
 export default class Index extends Component {
   render() {
     return (
-      <Home />
+      <Chat />
     );
   }
 }
--- App/routes/Chat.js
+++ App/routes/Chat.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 
-export default class Home extends Component {
+export default class Chat extends Component {
   render() {
     return (
       <View>
         <Text>
-          Home route
+          Chat route
         </Text>
       </View>
     );
