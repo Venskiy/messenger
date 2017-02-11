bb7dfa28ea9818718d3cc8d4d4d4f6a354cfda8d
vensilya@gmail.com <vensilya@gmail.com>
2017-02-11 13:55:01 +0300

    Create saga middleware

--- App/index.js
+++ App/index.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
 import {StyleSheet, View, Text, NavigatorIOS} from 'react-native';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
 
 import * as reducers from './reducers';
+import mySaga from './sagas';
 import Home from './routes/Home';
 import Chat from './routes/Chat';
 
+const sagaMiddleware = createSagaMiddleware()
 const reducer = combineReducers(reducers);
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(mySaga);
 
 export default class Index extends Component {
   render() {
---
+++ App/sagas/index.js
@@ -0,0 +1,6 @@
+import { call, put, takeLatest } from 'redux-saga/effects'
+
+function* mySaga() {
+}
+
+export default mySaga;
